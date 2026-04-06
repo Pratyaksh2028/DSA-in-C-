558e3ecf27dee349e9879fb7713d044803241fa9
Pratyaksh2028 <pratyakshrawat2028@gmail.com>
2026-04-06 13:54:35 +0530

    Update palindrome.cpp

--- Strings/Strings/palindrome.cpp
+++ Strings/Strings/palindrome.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string s = "madam";
+
+    int start = 0;
+    int end = s.length() - 1;
+
+    bool isPalindrome = true;
+
+    while(start < end) {
+
+        // compare both ends
+        if(s[start] != s[end]) {
+            isPalindrome = false;
+            break;
+        }
+
+        start++;
+        end--;
+    }
+
+    if(isPalindrome)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+}
