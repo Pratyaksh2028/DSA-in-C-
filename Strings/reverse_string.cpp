47551c687be495ac46bf52ce9501fba2f1d695fd
Pratyaksh2028 <pratyakshrawat2028@gmail.com>
2026-04-06 13:50:44 +0530

    Update reverse_string.cpp

--- Strings/reverse_string.cpp
+++ Strings/reverse_string.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string s = "hello";
+
+    int start = 0;
+    int end = s.length() - 1;
+
+    while(start < end) {
+        swap(s[start], s[end]);
+        start++;
+        end--;
+    }
+
+    cout << s;
+}
